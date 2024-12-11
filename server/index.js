4d0b3cd0a7a4d5cbee28692437db2af1a66aabac
Nav73 <nhued1@gmail.com>
2024-12-11 13:29:24 +0530

    git-ignore added in server

--- server/index.js
+++ server/index.js
@@ -1,18 +1,21 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
-
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename)
-const PORT = 8000;
+// const PORT = 8000;
+const PORT = process.env.PORT;
+
 
 app.get("/", (req, res)=>{
 res.sendFile(path.join(__dirname, "/views/hello.html"));
 })
 
 app.listen(PORT, ()=>{
-    console.log("server running http://localhost:8000")
+    console.log(`server running http://localhost:${PORT}`)
 })
