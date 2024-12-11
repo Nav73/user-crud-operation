60660b18da914a4b09ace3205034ad3e13a63246
Nav73 <nhued1@gmail.com>
2024-12-11 13:12:21 +0530

    views added

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
-
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
 const PORT = 8000;
 
 app.get("/", (req, res)=>{
-    res.sendFile(__dirname + "hello.html");
+res.sendFile(path.join(__dirname, "/views/hello.html"));
 })
 
 app.listen(PORT, ()=>{
